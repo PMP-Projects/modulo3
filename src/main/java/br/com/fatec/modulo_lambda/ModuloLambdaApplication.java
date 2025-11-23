aef50165f3f52a1f0ff60af72e6e100f8f353952
juliosn <juliodsneves@hotmail.com>
2025-11-23 12:15:59 -0300

    feat: initializng project

---
+++ src/main/java/br/com/fatec/modulo_lambda/ModuloLambdaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.modulo_lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/modulo_lambda/ModuloLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.modulo_lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
